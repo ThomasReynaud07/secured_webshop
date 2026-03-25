4f11b25b03cbc4236c9ea41e0548f3f173781995
Thomas <pk84vtk@eduvaud.ch>
2026-03-25 12:13:54 +0100

    feat: sel, poivre, requet

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,10 +1,11 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   // ----------------------------------------------------------
   // POST /api/auth/login
   // ----------------------------------------------------------
-  login: (req, res) => {
+  login: async (req, res) => {
     console.log(req.body);
     const { username, password } = req.body;
 
@@ -12,9 +13,9 @@ module.exports = {
       return res.status(400).json({ error: "Email et mot de passe requis" });
     }
 
-    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+    const query = `SELECT * FROM users WHERE username = ? `;
 
-    db.query(query, (err, results) => {
+    db.query(query, [username], async (err, results) => {
       if (err) {
         return res.status(500).json({ error: err.message, query: query });
       }
@@ -24,24 +25,31 @@ module.exports = {
           .status(401)
           .json({ error: "Email ou mot de passe incorrect" });
       }
-
+      const user = results[0];
+      const passwordPeper = password + process.env.SECRETKEY;
+      const hashedpassword = await bcrypt.compare(passwordPeper, user.password);
+      if (!hashedpassword) {
+        return res.status(401).json({ error: "hasba" });
+      }
       //res.json({ message: "Connexion réussie", user: results[0] });
-      res.redirect("/");
       req.flash("success", "Connexion réussie");
+      res.redirect("/");
     });
   },
 
   // ----------------------------------------------------------
   // POST /api/auth/register
   // ----------------------------------------------------------
-  register: (req, res) => {
+  register: async (req, res) => {
     const { username, email, password } = req.body;
     if (!username || !password) {
       return res.status(400).json({ error: "Email et Mot de passe requis" });
     }
+    const passwordPeper = password + process.env.SECRETKEY;
+    const hashedpassword = await bcrypt.hash(passwordPeper, 10);
     const query = `INSERT INTO users (username, email, password) VALUES (?, ?, ?) `;
 
-    db.query(query, [username, email, password], (err, results) => {
+    db.query(query, [username, email, hashedpassword], (err, results) => {
       if (err) {
         return res.status(500).json({ error: err.message, query: query });
       }
