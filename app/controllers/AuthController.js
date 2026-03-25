d8fab8108abaee0b27de98cbd83ddc9a78f26a06
Thomas <pk84vtk@eduvaud.ch>
2026-03-25 11:49:19 +0100

    feat: register

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -5,7 +5,7 @@ module.exports = {
   // POST /api/auth/login
   // ----------------------------------------------------------
   login: (req, res) => {
-    console.log(req);
+    console.log(req.body);
     const { username, password } = req.body;
 
     if (!username || !password) {
@@ -34,7 +34,25 @@ module.exports = {
   // ----------------------------------------------------------
   // POST /api/auth/register
   // ----------------------------------------------------------
-  register: (_req, res) => {
-    res.status(501).json({ error: "Non implémenté — TODO exercice 7" });
+  register: (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ error: "Email et Mot de passe requis" });
+    }
+    const query = `INSERT INTO users (username, email, password) VALUES (?, ?, ?) `;
+
+    db.query(query, [username, email, password], (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: err.message, query: query });
+      }
+
+      if (results.length === 0) {
+        return res
+          .status(401)
+          .json({ error: "Email ou mot de passe incorrect" });
+      }
+
+      res.redirect("/login");
+    });
   },
 };
